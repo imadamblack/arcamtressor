8b7d3a8d1a0a0b68c24175d12315de7f07d48889
Adam Black <fernando@notoriovs.com>
2024-08-02 14:56:15 -0600

    UPD: Header Logo

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { info } from '../../../info';
 import Image from 'next/image';
-import logo from '../../../public/logo.png';
+import logo from '../../../public/logo-h.png';
 
 export default function Header() {
   return (
@@ -10,9 +10,9 @@ export default function Header() {
       className={`sticky top-0 bg-neutral-800 backdrop-blur-lg border-b-4 border-brand-1 w-screen shadow-sm h-[8rem] flex justify-center z-[99] hover:top-0`}
     >
       <div className="flex items-center z-[1]">
-        <div className="relative flex justify-center items-center w-[12rem]">
+        <div className="relative flex justify-center items-center w-[24rem]">
           <Link href="/" passhref>
-            <Image src={logo} alt={info.companyName} objectFit="contain"/>
+            <Image src={logo} alt={info.companyName} objectFit="contain" />
           </Link>
         </div>
       </div>
